ac718f3d85d0098bc8a6a242e44660d20b4d991a
arnaud422 <48210106+arnaud422@users.noreply.github.com>
2022-06-29 21:31:00 +0100

    player

--- src/js/index.js
+++ src/js/index.js
@@ -26,14 +26,47 @@ class Wall {
   }
 
   draw() {
-    console.log(this.position);
     ctx.fillStyle = "blue";
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 }
 
+//Player obj
+class Player {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = 15;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "yellow";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
 (() => {
   const walls = [];
+
+  const player = new Player({
+    position: {
+      x: Wall.width + Wall.width / 2,
+      y: Wall.height + Wall.height / 2,
+    },
+    velocity: {
+      x: 0,
+      y: 0,
+    },
+  });
 
   //add walls in [walls] for show walls
   MAP.forEach((row, i) => {
@@ -52,8 +85,45 @@ class Wall {
       }
     });
   });
+
+  //   Function show walls
+  function wallsDraw() {
+    walls.forEach((wall) => {
+      wall.draw();
+    });
+  }
+
   //   show walls
-  walls.forEach((wall) => {
-    wall.draw();
+  wallsDraw();
+
+  //afficher le joueur
+  player.draw();
+
+  //gestion des touches
+
+  document.addEventListener("keyup", ({ key }) => {
+    console.log(key)
+    switch (key) {
+      case "z":
+        player.velocity.y += -5;
+        break;
+      case "q":
+        player.velocity.x += -5;
+        break;
+      case "s":
+        player.velocity.y += 5;
+        break;
+      case "d":
+        player.velocity.x += 5;
+        break;
+    }
   });
+
+  function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, GAME.width, GAME.height);
+    wallsDraw()
+    player.update();
+  }
+  animate();
 })();
